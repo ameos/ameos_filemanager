fb570495ce49833cd1dd648bc48026df5c9041bd
Ameos <typo3dev@ameos.com>
2017-03-02 11:02:51 +0100

    [FEATURE] Scheduled tasks for initialize or recalculate status

--- ext_localconf.php
+++ ext_localconf.php
@@ -5,6 +5,7 @@ use TYPO3\CMS\Extbase\Utility\ExtensionUtility;
 use TYPO3\CMS\Filelist\FileList;
 use Ameos\AmeosFilemanager\Hooks\ProcessDatamap;
 use Ameos\AmeosFilemanager\XClass\FileList as XClassFileList;
+use Ameos\AmeosFilemanager\ScheduledTasks\CacheStatus;
 
 // plugin
 ExtensionUtility::configurePlugin('Ameos.' . $_EXTKEY, 'fe_filemanager',
@@ -25,3 +26,10 @@ $GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects'][FileList::class] = ['className' =>
 
 // hooks
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass'][ProcessDatamap::class] = 'EXT:ameos_filemanager/Classes/Hooks/ProcessDatamap.php:' . ProcessDatamap::class;
+
+// scheduler
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['scheduler']['tasks'][CacheStatus::class] = [
+    'extension'        => $_EXTKEY,
+    'title'            => 'LLL:EXT:' . $_EXTKEY . '/Resources/Private/Language/locallang_be.xlf:scheduler.cachestatus.title',
+    'description'      => 'LLL:EXT:' . $_EXTKEY . '/Resources/Private/Language/locallang_be.xlf:scheduler.cachestatus.description',
+];

d54aa2b2a1f0dffa157138ea1a6aebe93492bde7
Ameos <typo3dev@ameos.com>
2017-03-23 14:19:00 +0100

    [TASK] Update number version (1.1.6)

--- ext_emconf.php
+++ ext_emconf.php
@@ -21,7 +21,7 @@ $EM_CONF[$_EXTKEY] = [
     'uploadfolder'     => false,
     'createDirs'       => '',
     'clearCacheOnLoad' => 0,
-    'version'          => '1.1.5',
+    'version'          => '1.1.6',
     'clearcacheonload' => false,
     'constraints'      => [
         'depends'   => ['typo3' => '7.6.0-8.7.99', 'php' => '5.5.0-7.0.99'],

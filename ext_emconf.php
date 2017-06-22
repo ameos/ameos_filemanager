5920ecdc4ce9c66ac0b28174aece8bca338702ca
Ameos <typo3dev@ameos.com>
2017-06-22 15:48:40 +0200

    [BUGFIX] Oups. Stupid parse error

--- ext_emconf.php
+++ ext_emconf.php
@@ -23,7 +23,7 @@ $EM_CONF[$_EXTKEY] = [
     'clearCacheOnLoad' => 0,
     'version'          => '1.1.9',
     'clearcacheonload' => false,
-    'autoload'         => ['psr-4' => ['Ameos\\AmeosFilemanager\\' => 'Classes']]
+    'autoload'         => ['psr-4' => ['Ameos\\AmeosFilemanager\\' => 'Classes']],
     'constraints'      => [
         'depends'   => ['typo3' => '7.6.0-8.7.99', 'php' => '5.5.0-7.0.99'],
         'conflicts' => [],

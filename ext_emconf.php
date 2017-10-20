5b9cf610cbb638132efeda1e4d4e52c6575e98ef
Ameos <typo3dev@ameos.com>
2017-10-20 10:24:38 +0200

    [BUGFIX] Remove use instruction in ext_tables and ext_localconf. This can cause conflict

--- ext_emconf.php
+++ ext_emconf.php
@@ -21,7 +21,7 @@ $EM_CONF[$_EXTKEY] = [
     'uploadfolder'     => false,
     'createDirs'       => '',
     'clearCacheOnLoad' => 0,
-    'version'          => '1.1.9',
+    'version'          => '1.1.10',
     'clearcacheonload' => false,
     'autoload'         => ['psr-4' => ['Ameos\\AmeosFilemanager\\' => 'Classes']],
     'constraints'      => [
--- ext_tables.php
+++ ext_tables.php
@@ -1,57 +1,47 @@
 <?php
 if (!defined('TYPO3_MODE')) { die ('Access denied.'); }
 
-use TYPO3\CMS\Core\Utility\GeneralUtility;
-use TYPO3\CMS\Extbase\Utility\ExtensionUtility;
-use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
-use TYPO3\CMS\Extbase\Object\ObjectManager;
-use TYPO3\CMS\Core\Resource\ResourceStorage;
-use TYPO3\CMS\Extbase\SignalSlot\Dispatcher;
-use TYPO3\CMS\Core\Imaging\IconRegistry;
-use TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider;
-use Ameos\AmeosFilemanager\Slots\Slot;
-
 // register plugin
-ExtensionUtility::registerPlugin('Ameos.' . $_EXTKEY, 'fe_filemanager',
+\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin('Ameos.' . $_EXTKEY, 'fe_filemanager',
     'LLL:EXT:ameos_filemanager/Resources/Private/Language/locallang_be.xlf:plugin.fe_filemanager.title');
-ExtensionUtility::registerPlugin('Ameos.' . $_EXTKEY, 'fe_filemanager_export',
+\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin('Ameos.' . $_EXTKEY, 'fe_filemanager_export',
     'LLL:EXT:ameos_filemanager/Resources/Private/Language/locallang_be.xlf:plugin.fe_filemanager_export.title');
-ExtensionUtility::registerPlugin('Ameos.' . $_EXTKEY, 'fe_filemanager_search',
+\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin('Ameos.' . $_EXTKEY, 'fe_filemanager_search',
     'LLL:EXT:ameos_filemanager/Resources/Private/Language/locallang_be.xlf:plugin.fe_filemanager_search.title');
-ExtensionUtility::registerPlugin('Ameos.' . $_EXTKEY, 'fe_filemanager_flat',
+\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin('Ameos.' . $_EXTKEY, 'fe_filemanager_flat',
     'LLL:EXT:ameos_filemanager/Resources/Private/Language/locallang_be.xlf:plugin.fe_filemanager_flat.title');
 
 //Flexforms
 $TCA['tt_content']['types']['list']['subtypes_excludelist']['ameosfilemanager_fe_filemanager'] = 'layout,select_key,recursive';
 $TCA['tt_content']['types']['list']['subtypes_addlist']['ameosfilemanager_fe_filemanager']     = 'pi_flexform';
-ExtensionManagementUtility::addPiFlexFormValue('ameosfilemanager_fe_filemanager', 'FILE:EXT:'. $_EXTKEY . '/Configuration/FlexForms/filemanager.xml');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue('ameosfilemanager_fe_filemanager', 'FILE:EXT:'. $_EXTKEY . '/Configuration/FlexForms/filemanager.xml');
 
 $TCA['tt_content']['types']['list']['subtypes_excludelist']['ameosfilemanager_fe_filemanager_export'] = 'layout,select_key,recursive';
 $TCA['tt_content']['types']['list']['subtypes_addlist']['ameosfilemanager_fe_filemanager_export']     = 'pi_flexform';
-ExtensionManagementUtility::addPiFlexFormValue('ameosfilemanager_fe_filemanager_export', 'FILE:EXT:'. $_EXTKEY . '/Configuration/FlexForms/export.xml');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue('ameosfilemanager_fe_filemanager_export', 'FILE:EXT:'. $_EXTKEY . '/Configuration/FlexForms/export.xml');
 
 $TCA['tt_content']['types']['list']['subtypes_excludelist']['ameosfilemanager_fe_filemanager_search'] = 'layout,select_key,recursive';
 $TCA['tt_content']['types']['list']['subtypes_addlist']['ameosfilemanager_fe_filemanager_search']     = 'pi_flexform';
-ExtensionManagementUtility::addPiFlexFormValue('ameosfilemanager_fe_filemanager_search', 'FILE:EXT:'. $_EXTKEY . '/Configuration/FlexForms/search.xml');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue('ameosfilemanager_fe_filemanager_search', 'FILE:EXT:'. $_EXTKEY . '/Configuration/FlexForms/search.xml');
 
 $TCA['tt_content']['types']['list']['subtypes_excludelist']['ameosfilemanager_fe_filemanager_flat'] = 'layout,select_key,recursive';
 $TCA['tt_content']['types']['list']['subtypes_addlist']['ameosfilemanager_fe_filemanager_flat']     = 'pi_flexform';
-ExtensionManagementUtility::addPiFlexFormValue('ameosfilemanager_fe_filemanager_flat', 'FILE:EXT:'. $_EXTKEY . '/Configuration/FlexForms/flatlist.xml');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue('ameosfilemanager_fe_filemanager_flat', 'FILE:EXT:'. $_EXTKEY . '/Configuration/FlexForms/flatlist.xml');
 
 // Typoscript
-ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript/', 'File manager > Default');
-ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript/Ajax/', 'File manager > Activate Ajax Mode (required jquery)');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript/', 'File manager > Default');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript/Ajax/', 'File manager > Activate Ajax Mode (required jquery)');
 
 // Register icons
-$iconRegistry = GeneralUtility::makeInstance(IconRegistry::class);
-$iconRegistry->registerIcon('extension-ameosfilemanager-main', SvgIconProvider::class, [
+$iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Imaging\IconRegistry::class);
+$iconRegistry->registerIcon('extension-ameosfilemanager-main', \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class, [
     'source' => 'EXT:ameos_filemanager/Resources/Public/IconsBackend/folder.svg'
 ]);
 
 /**
  * ContentElementWizard
  */
-ExtensionManagementUtility::addPageTSConfig(
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
     '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:ameos_filemanager/Configuration/TSConfig/ContentElementWizard.tsconfig">'
 );
 
@@ -65,18 +55,18 @@ if (TYPO3_MODE == 'BE') {
         );        
     }
     
-    // Slots
-    $dispatcher = GeneralUtility::makeInstance(ObjectManager::class)->get(Dispatcher::class);
+    // \Ameos\AmeosFilemanager\Slots\Slots
+    $dispatcher = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Extbase\Object\ObjectManager::class)->get(\TYPO3\CMS\Extbase\SignalSlot\Dispatcher::class);
 
-    // Folders slots
-    $dispatcher->connect(ResourceStorage::class, 'postFolderRename', Slot::class, 'postFolderRename');
-    $dispatcher->connect(ResourceStorage::class, 'postFolderAdd',    Slot::class, 'postFolderAdd');
-    $dispatcher->connect(ResourceStorage::class, 'postFolderMove',   Slot::class, 'postFolderMove');
-    $dispatcher->connect(ResourceStorage::class, 'postFolderCopy',   Slot::class, 'postFolderCopy');
-    $dispatcher->connect(ResourceStorage::class, 'postFolderDelete', Slot::class, 'postFolderDelete');
+    // Folders \Ameos\AmeosFilemanager\Slots\Slots
+    $dispatcher->connect(\TYPO3\CMS\Core\Resource\ResourceStorage::class, 'postFolderRename', \Ameos\AmeosFilemanager\Slots\Slot::class, 'postFolderRename');
+    $dispatcher->connect(\TYPO3\CMS\Core\Resource\ResourceStorage::class, 'postFolderAdd',    \Ameos\AmeosFilemanager\Slots\Slot::class, 'postFolderAdd');
+    $dispatcher->connect(\TYPO3\CMS\Core\Resource\ResourceStorage::class, 'postFolderMove',   \Ameos\AmeosFilemanager\Slots\Slot::class, 'postFolderMove');
+    $dispatcher->connect(\TYPO3\CMS\Core\Resource\ResourceStorage::class, 'postFolderCopy',   \Ameos\AmeosFilemanager\Slots\Slot::class, 'postFolderCopy');
+    $dispatcher->connect(\TYPO3\CMS\Core\Resource\ResourceStorage::class, 'postFolderDelete', \Ameos\AmeosFilemanager\Slots\Slot::class, 'postFolderDelete');
 
-    // Files slots
-    $dispatcher->connect(ResourceStorage::class, 'postFileAdd',  Slot::class, 'postFileAdd');
-    $dispatcher->connect(ResourceStorage::class, 'postFileCopy', Slot::class, 'postFileCopy');
-    $dispatcher->connect(ResourceStorage::class, 'postFileMove', Slot::class, 'postFileMove');
+    // Files \Ameos\AmeosFilemanager\Slots\Slots
+    $dispatcher->connect(\TYPO3\CMS\Core\Resource\ResourceStorage::class, 'postFileAdd',  \Ameos\AmeosFilemanager\Slots\Slot::class, 'postFileAdd');
+    $dispatcher->connect(\TYPO3\CMS\Core\Resource\ResourceStorage::class, 'postFileCopy', \Ameos\AmeosFilemanager\Slots\Slot::class, 'postFileCopy');
+    $dispatcher->connect(\TYPO3\CMS\Core\Resource\ResourceStorage::class, 'postFileMove', \Ameos\AmeosFilemanager\Slots\Slot::class, 'postFileMove');
 }

b97eda58488cf7f332a3c39b1087d7f0d279d6b6
Ameos <typo3dev@ameos.com>
2018-04-30 16:02:31 +0200

    [TASK] Update version number

--- ext_emconf.php
+++ ext_emconf.php
@@ -21,7 +21,7 @@ $EM_CONF[$_EXTKEY] = [
     'uploadfolder'     => false,
     'createDirs'       => '',
     'clearCacheOnLoad' => 0,
-    'version'          => '1.3.1',
+    'version'          => '1.3.2',
     'clearcacheonload' => false,
     'autoload'         => ['psr-4' => ['Ameos\\AmeosFilemanager\\' => 'Classes']],
     'constraints'      => [

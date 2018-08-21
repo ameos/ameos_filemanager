40bc180d640527c54143a2c3512e8f1db6584ea0
Ameos <typo3dev@ameos.com>
2018-08-21 12:01:07 +0200

    [TASK] Version update

--- ext_emconf.php
+++ ext_emconf.php
@@ -21,11 +21,11 @@ $EM_CONF[$_EXTKEY] = [
     'uploadfolder'     => false,
     'createDirs'       => '',
     'clearCacheOnLoad' => 0,
-    'version'          => '1.3.3',
+    'version'          => '1.3.4',
     'clearcacheonload' => false,
     'autoload'         => ['psr-4' => ['Ameos\\AmeosFilemanager\\' => 'Classes']],
     'constraints'      => [
-        'depends'   => ['typo3' => '7.6.0-8.7.99', 'php' => '5.6.0-7.0.99'],
+        'depends'   => ['typo3' => '7.6.0-8.7.99', 'php' => '5.6.0-7.2.99'],
         'conflicts' => [],
         'suggests'  => [],
     ],

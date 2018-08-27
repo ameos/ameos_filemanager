0d131fdd60cd4748b1a9e3c0d04973c142ce0f2d
Ameos <typo3dev@ameos.com>
2018-08-27 10:38:45 +0200

    [BUGFIX] Bug of the configuration display mode

--- Classes/Controller/Explorer/ExplorerController.php
+++ Classes/Controller/Explorer/ExplorerController.php
@@ -76,17 +76,19 @@ class ExplorerController extends AbstractController
             );
         }
 
+        $availableMode = is_array($this->settings['availableMode']) ? $this->settings['availableMode'] : GeneralUtility::trimExplode(',', $this->settings['availableMode']);
+        if (is_array($this->settings['availableMode'])) {
+            $this->settings['availableMode'] = implode(',', $this->settings['availableMode']);
+        }
+        
         // get current display mode
-        $displayMode = ExplorerUtility::getDisplayMode(
-            $this->settings['availableMode'],
-            $this->configurationManager->getContentObject()->data['uid']
-        );
+        $displayMode = ExplorerUtility::getDisplayMode($availableMode, $this->configurationManager->getContentObject()->data['uid']);
 
         // assign data to the view
         $this->view->assign('root_folder', $rootFolder);
         $this->view->assign('current_folder', $currentFolder);
         $this->view->assign('files', $this->fileRepository->findFilesForFolder($currentFolder->getUid(), $configuration['view']['pluginNamespace']));
-        $this->view->assign('has_many_display_mode', (strpos($this->settings['availableMode'], ',') !== false));
+        $this->view->assign('has_many_display_mode', (count($availableMode) > 1));
         $this->view->assign('display_mode', $displayMode);        
         $this->view->assign('columns_table', GeneralUtility::trimExplode(',', $this->settings['columnsTable']));
         $this->view->assign('allowed_actions_files', GeneralUtility::trimExplode(',', $this->settings['allowedActionsOnFiles']));

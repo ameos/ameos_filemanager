a767790806c75ade1cb0ce3e8a224499c95199d2
Ameos <typo3dev@ameos.com>
2018-11-20 16:06:00 +0100

    [BUGFIX] bug on file download with space

--- Classes/Utility/DownloadUtility.php
+++ Classes/Utility/DownloadUtility.php
@@ -150,7 +150,7 @@ class DownloadUtility
                 // Download of the file
                 header('Content-Description: File Transfer');
                 header('Content-Type: ' . mime_content_type($filename));
-                header('Content-Disposition: attachment; filename=' . basename($filename));
+                header('Content-Disposition: attachment; filename="' . basename($filename) . '"');
                 header('Expires: 0');
                 header('Cache-Control: must-revalidate');
                 header('Pragma: public');

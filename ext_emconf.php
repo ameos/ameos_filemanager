82e7c924c40557609c5b250d7082b46328ecaee8
Stéphane Fillion <s.fillion@ameos.com>
2019-04-12 09:51:11 +0200

    [TASK] Update version for release

--- ext_emconf.php
+++ ext_emconf.php
@@ -21,7 +21,7 @@ $EM_CONF[$_EXTKEY] = [
     'uploadfolder'     => false,
     'createDirs'       => '',
     'clearCacheOnLoad' => 0,
-    'version'          => '1.4.0',
+    'version'          => '1.4.1',
     'clearcacheonload' => false,
     'autoload'         => ['psr-4' => ['Ameos\\AmeosFilemanager\\' => 'Classes']],
     'constraints'      => [
